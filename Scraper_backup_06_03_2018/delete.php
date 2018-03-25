e37e19c5a65fc84e9b2acffc88044d51724aadea
Jenny Vega <jelusita11@gmail.com>
2018-03-25 11:37:24 -0500

    delete all the products everyday

--- Scraper_backup_06_03_2018/delete.php
+++ Scraper_backup_06_03_2018/delete.php
@@ -6,6 +6,7 @@ include("includes/DatabaseConnection.php");
 include("permission.php");
 
 $id = $_POST["delete_id"];
-$get_file = "delete from product where product_id='".$id."'";
+#$get_file = "delete from product where product_id='".$id."'";
+$get_file = "delete from product";
 $run_file = mysqli_query($mysqli,$get_file) or die($mysqli);
 ?>
